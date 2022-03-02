a109728493f5bc4fe1873c87240be59ab27b8439
tsai <viabytsai@gmail.com>
2022-03-02 21:47:53 +0800

    update

--- dist/main.dfc06931.js
+++ dist/main.dfc06931.js
@@ -118,8 +118,6 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"epB2":[function(require,module,exports) {
-if (document.documentElement.clientWidth < 500) {
-  window.alert('请使用电脑浏览器打开，本页面暂未适配手机端');
-}
+
 },{}]},{},["epB2"], null)
-//# sourceMappingURL=main.2bed32e8.js.map
+//# sourceMappingURL=main.dfc06931.js.map
--- src/main.js
+++
@@ -1,3 +0,0 @@
-if(document.documentElement.clientWidth<500){
-    window.alert('请使用电脑浏览器打开，本页面暂未适配手机端')
-}
